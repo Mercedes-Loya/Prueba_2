657fa87e2f87698dd23e16d2903382148537a812
Mercedes-Loya <maria_loya27@yahoo.com>
2018-10-24 12:02:32 -0500

    Este es una primera Version del proyecto colaborativo

---
+++ src/com/mloya/vista/Aplicacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mloya.vista;
+
+/**
+ *
+ * @author MARIA LOYA
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Prueba_2");
+    }
+    
+}
